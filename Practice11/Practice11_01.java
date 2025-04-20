f85e4e8e3b79229c346b1e0323162d66394a39fc
haruka0429 <pudding.p3208@gmail.com>
2025-04-20 21:02:07 +0900

    Add files via upload

---
+++ Practice11/Practice11_01.java
@@ -0,0 +1,14 @@
+package Practice11;
+public class Practice11_01 {
+    public static void main(String[] args) {
+        int i = 1;
+        int total = 0;
+        while (i < 11) {
+            total += i;
+            i++;
+        }
+        System.out.println("1～10の合計は" + total + "です");
+
+    }
+    
+}
