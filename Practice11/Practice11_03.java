d56d009138bc8303b5829cc23a324f5101b19d5e
haruka0429 <pudding.p3208@gmail.com>
2025-04-26 19:25:06 +0900

    Add files via upload

---
+++ Practice11/Practice11_03.java
@@ -0,0 +1,18 @@
+package Practice11;
+import java.util.Random;
+
+public class Practice11_03 {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 10) {
+            int num = new Random().nextInt(6) + 1;
+            //System.out.println(num);
+            if (num % 2 != 0) {
+                System.out.println((x + 1) + "回目、" + num + "が出ました");
+            }
+            x++;    
+        }
+            
+    }
+    
+}
