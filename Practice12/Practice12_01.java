5ab735795b75f84f004d2c46502f9491d2a5b6d0
haruka0429 <pudding.p3208@gmail.com>
2025-04-28 19:46:33 +0900

    Add files via upload

---
+++ Practice12/Practice12_01.java
@@ -0,0 +1,10 @@
+public class Practice12_01 {
+    public static void main(String[] args) {
+        add(100, 50);
+
+    }
+    public static void add(int x, int y) {
+        int ans = x - y;
+        System.out.println(x + "-" + y + "=" + ans);
+    }
+}
